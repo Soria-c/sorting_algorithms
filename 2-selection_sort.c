b34562f22de33a5f8df46c35b3bb3e4a3c9ab309
Carlos Soria <soriac367@gmail.com>
2022-06-14 14:10:25 -0500

    Added null handler

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,7 +13,7 @@ void selection_sort(int *array, size_t size)
 	if (!array || size <= 1)
 		return;
 
-	for (i = 0; i < size - 2; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
